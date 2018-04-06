0728f830a41eba896b8568843aa08a35b2d267f9
shermelee <38107674+shermelee@users.noreply.github.com>
2018-04-06 21:39:15 +0530

    Add files via upload

---
+++ solution.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author New
+ */
+public class solution {
+    public static void main(String arg[]){
+        System.out.println("enter the string");
+        Scanner s=new Scanner(System.in);
+        s.nextLine();
+        s.close();
+        System.out.println("Hello, World.");
+        System.out.println(+s);
+    }
+    
+}
